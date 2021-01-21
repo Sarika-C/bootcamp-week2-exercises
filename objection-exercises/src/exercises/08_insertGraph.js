8889fe0138b1fe992027e7ddf4d6d42a30005fa6
Sarika Chawla <isarikachawla@gmail.com>
2021-01-21 05:30:27 -0500

    Completed Exercise 08

--- objection-exercises/src/exercises/08_insertGraph.js
+++ objection-exercises/src/exercises/08_insertGraph.js
@@ -1,11 +1,30 @@
 const cleanup = require('../lib/cleanup')
 // Import models
+const User = require('../models/User')
+const Pet = require('../models/Pet')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Insert a new person name Peter Bynum with two pet DOGs named Rocco & Rosey
+  const peter = await User.query().insertGraph({
+    email: 'peter_bynum@gmail.com',
+    firstName: 'Peter',
+    lastName: 'Bynum',
+    age: 55,
+    pets: [
+      {
+        type: 'DOG',
+        name: 'Rocco'
+      },
 
+      {
+        type: 'DOG',
+        name: 'Rosey'
+      }
+    ]
+  }).returning("*")
+  console.log(peter)
   // -----
   cleanup()
 }
